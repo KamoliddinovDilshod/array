2c580bd06fb2249b75552b9eaf4f021735c5eaab
KamoliddinovDilshod <dilshodkamoliddinov03@gmail.com>
2022-03-04 23:57:26 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var num = [18 , 23, 45, 64, 57, 89, 90];
+numbers = function(list){
+  var defoult = 0;
+  for (var i = 0; i< list.length; i++){
+    defoult+= list[i];
+  }
+  return defoult;
+}
+
+console.debug(numbers(num));
